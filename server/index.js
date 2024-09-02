929e78f7b8e4b8312a2e1573cec5f31cfd0f26f6
Lucifer <57283974+H1m9n5hu@users.noreply.github.com>
2024-09-02 15:36:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 app.use('/api/tasks', tasksRouter);
 
+app.get('/ping', async(req, res) => {
+  res.send('Welcome to ToDoList API');
+})
+
 // Server Setup
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
